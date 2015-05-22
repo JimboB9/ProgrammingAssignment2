656ce9a9e018dcbedd89d66594617f2da1ae9042
JimboB9 <ludviktor@gmail.com>
2015-05-22 15:23:48 +0200

    Upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve are functions that make sure that the inverse
+## of a (squared and invertible) matrix is only computed once to avoid multiple
+## computations of the same matrix, which is computationally expensive.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a (squared and invertible) matrix as input and returns 
+## a list of functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   I <- NULL
+    set <- function(y) {
+      x <<- y
+      I <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) I <<- inv
+  getinv <- function() I
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a makeCacheMatrix object as input and checks, if the inverse 
+## has already been computed. If yes, it displays "getting cashed data" and return
+## the inverse from the cache. If no, the inverse is computed, cached and displayed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+    I <- x$getinv()
+    if(!is.null(I)) {
+      message("getting cached data")
+      return(I)
+    }
+  data <- x$get()
+  I <- solve(data, ...)
+  x$setinv(I)
+  I  
 }
+
+
+## Test functions
+
+#l <- matrix(1:9, 3, 3, byrow=T)
+#diag(l) <- 0  # make sure matrix is invertible
+#lcache <- makeCacheMatrix(l)
+#cacheSolve(lcache)
+
+
+
+
+
